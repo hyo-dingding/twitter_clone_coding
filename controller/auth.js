faa3ed2e22572a1f7acb1136d4d8c28d73d7a18d
hyo-dingding <zvoniti2964@gmail.com>
2023-02-14 23:38:51 +0900

    config add

--- controller/auth.js
+++ controller/auth.js
@@ -2,11 +2,7 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import {} from "express-async-errors";
 import * as userRepository from "../data/auth.js";
-
-// TODO: Make it secure!
-const jwtSecretKey = "F2dN7x8HVzBWaQuEEDnhsvHXRWqAR63z";
-const jwtExpiresInDays = "2d";
-const bcryptSaltRounds = 12;
+import { config } from "../config.js";
 
 export async function signup(req, res) {
     const { username, password, name, email, url } = req.body;
@@ -15,7 +11,7 @@ export async function signup(req, res) {
         return res.status(409).json({ message: `${username} already exists` });
     }
 
-    const hashed = await bcrypt.hash(password, bcryptSaltRounds);
+    const hashed = await bcrypt.hash(password, config.bcrypt.saltRounds);
     const userId = await userRepository.createUser({
         username,
         password: hashed,
@@ -44,7 +40,7 @@ export async function login(req, res) {
     res.status(200).json({ token, username });
 }
 function createJwtToken(id) {
-    return jwt.sign({ id }, jwtSecretKey, { expiresIn: jwtExpiresInDays });
+    return jwt.sign({ id }, config.jwt.secretKey, { expiresIn: config.jwt.expiresInSec });
 }
 export async function me(req, res, next) {
     const user = await userRepository.findById(req.userId);
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import { config } from "../config.js";
 import * as userRepository from "../data/auth.js";
 
 const AUTH_ERROR = { message: "Authentication Error" };
@@ -12,7 +13,8 @@ export const isAuth = async (req, res, next) => {
     const token = authHeader.split(" ")[1];
     jwt.verify(
         token,
-        "F2dN7x8HVzBWaQuEEDnhsvHXRWqAR63z", //
+        config.jwt.secretKey,
+
         async (error, decoded) => {
             if (error) {
                 return res.status(401).json(AUTH_ERROR);
