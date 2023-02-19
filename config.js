b3dd1c163ed7f89492214fadce60e0fa381729b3
hyo-dingding <zvoniti2964@gmail.com>
2023-02-19 23:45:05 +0900

    db

--- config.js
+++ config.js
@@ -1,12 +1,29 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+function required(key, defaultValue = undefined) {
+    const value = process.env[key] || defaultValue;
+    if (value == null) {
+        throw new Error(`Key ${key} is undefined`);
+    }
+    return value;
+}
+
 export const config = {
     jwt: {
-        secretKey: process.env.JWT_SECRET,
-        expiresInSec: process.env.JWT_EXPIRES_SEC,
+        secretKey: required("JWT_SECRET"),
+        expiresInSec: parseInt(required("JWT_EXPIRES_SEC", 86400)),
     },
     bcrypt: {
-        saltRounds: process.env.BCRYPT_SALT_ROUNDS,
+        saltRounds: parseInt(required("BCRYPT_SALT_ROUNDS", 12)),
+    },
+    host: {
+        post: parseInt(required("HOST_PORT", 8080)),
+    },
+    db: {
+        host: required("DB_HOST"),
+        user: required("DB_USER"),
+        database: required("DB_DATABASE"),
+        password: required("DB_PASSWORD"),
     },
 };
--- data/auth.js
+++ data/auth.js
@@ -1,27 +1,61 @@
 // abcd1234 = $2b$10$jvRhVWzdM1EN4/say9DJ..H2dkWy8AtCCnjg842rs9xsJ9T5m/koq
 
-let users = [
-    {
-        id: "1",
-        username: "bob",
-        password: "$2b$10$jvRhVWzdM1EN4/say9DJ..H2dkWy8AtCCnjg842rs9xsJ9T5m/koq",
-        name: "Bob",
-        email: "bob@gmail.com",
-        // url: "",
-    },
-];
+import { db } from "../db/database.js";
+
+// let users = [
+//     {
+//         id: "1",
+//         username: "bob",
+//         password: "$2b$10$jvRhVWzdM1EN4/say9DJ..H2dkWy8AtCCnjg842rs9xsJ9T5m/koq",
+//         name: "Bob",
+//         email: "bob@gmail.com",
+//         // url: "",
+//     },
+//     {
+//         id: "2",
+//         username: "coc",
+//         password: "$2b$10$ktkmALVtaOlh9x95eKD7zO/4CXRVWu2uaxbF.KFTsvPrNbTQIgAhK",
+//         name: "Coc",
+//         email: "coc@gmail.com",
+//         // url: "",
+//     },
+// ];
 
 export async function findByUsername(username) {
-    return users.find((user) => user.username === username);
+    return (
+        db.execute("SELECT * FROM users WHERE username=?", [username]) / //
+        then((result) => {
+            console.log(result);
+            return result[0][0];
+        })
+    );
+
+    // return users.find((user) => user.username === username);
 }
 
 export async function findById(id) {
-    return users.find((user) => user.id === id);
+    return (
+        db.execute("SELECT * FROM users WHERE id=?", [id]) / //
+        then((result) => {
+            console.log(result);
+            return result[0][0];
+        })
+    );
+    // return users.find((user) => user.id === id);
 }
 
 export async function createUser(user) {
-    const created = { ...user, id: new Date().toString() };
-    users.push(created);
+    const { username, password, name, email, url } = user;
+    return db
+        .execute("INSERT INTO users (username, password, name, email, url) VALUES (?,?,?,?,?)", [username, password, name, email, url])
 
-    return created.id;
+        .then((result) => {
+            console.log("&&", result);
+            return result;
+        });
+
+    // const created = { ...user, id: new Date().toString() };
+    // users.push(created);
+    // console.log("ddd", created);
+    // return created.id;
 }
